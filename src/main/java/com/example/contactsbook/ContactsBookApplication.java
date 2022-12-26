e57010417270bd33b2c9464b717cb799e5c9a435
tobi <spuraigor@gmail.com>
2022-12-26 22:16:22 +0200

    100% done

---
+++ src/main/java/com/example/contactsbook/ContactsBookApplication.java
@@ -0,0 +1,15 @@
+package com.example.contactsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+public class ContactsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsBookApplication.class, args);
+    }
+
+}
